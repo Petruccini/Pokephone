be5e2815d9d03ffb297390ef97fde50dd7e2dc10
petruccini <pdpdelfin@gmail.com>
2023-06-27 15:30:42 -0600

    Use flowOn(Dispatchers.IO) in data sources

--- app/src/main/java/com/petruccini/pokephone/data/data_sources/remote/RemotePokemonDetailsDataSource.kt
+++ app/src/main/java/com/petruccini/pokephone/data/data_sources/remote/RemotePokemonDetailsDataSource.kt
@@ -2,17 +2,17 @@ package com.petruccini.pokephone.data.data_sources.remote
 
 import com.petruccini.pokephone.data.api.services.pokemon_details.PokemonDetailsService
 import com.petruccini.pokephone.data.api.services.pokemon_details.model.toPokemonDetails
-import com.petruccini.pokephone.domain.entities.PokemonDetails
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RemotePokemonDetailsDataSource @Inject constructor(
     private val pokemonDetailsService: PokemonDetailsService
 ) {
 
-    suspend fun fetchPokemonDetails(pokemonName: String): Flow<PokemonDetails?> {
+    suspend fun fetchPokemonDetails(pokemonName: String) = flow {
         val response = pokemonDetailsService.getPokemonDetails(pokemonName)
-        return flowOf(response.body()?.toPokemonDetails())
-    }
+        emit(response.body()?.toPokemonDetails())
+    }.flowOn(Dispatchers.IO)
 }
--- app/src/main/java/com/petruccini/pokephone/data/data_sources/remote/RemotePokemonListDataSource.kt
+++ app/src/main/java/com/petruccini/pokephone/data/data_sources/remote/RemotePokemonListDataSource.kt
@@ -2,17 +2,17 @@ package com.petruccini.pokephone.data.data_sources.remote
 
 import com.petruccini.pokephone.data.api.services.pokemon_list.PokemonListService
 import com.petruccini.pokephone.data.api.services.pokemon_list.model.toPokemonList
-import com.petruccini.pokephone.domain.entities.PokemonList
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RemotePokemonListDataSource @Inject constructor(
     private val pokemonListService: PokemonListService
 ) {
 
-    suspend fun fetchPokemonList(offset: Int, limit: Int): Flow<PokemonList?> {
+    suspend fun fetchPokemonList(offset: Int, limit: Int) = flow {
         val response = pokemonListService.getPokemons(offset, limit)
-        return flowOf(response.body()?.toPokemonList())
-    }
+        emit(response.body()?.toPokemonList())
+    }.flowOn(Dispatchers.IO)
 }
