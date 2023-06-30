77e4c9f59a2308840bd291d6ca8dbbbc8bf14d41
petruccini <pdpdelfin@gmail.com>
2023-06-29 19:00:26 -0600

    Implement IO Dispatcher in DataSources

--- app/src/main/java/com/petruccini/pokephone/data/data_sources/remote/RemotePokemonDetailsDataSource.kt
+++ app/src/main/java/com/petruccini/pokephone/data/data_sources/remote/RemotePokemonDetailsDataSource.kt
@@ -1,13 +1,19 @@
 package com.petruccini.pokephone.data.data_sources.remote
 
+import com.petruccini.pokephone.config.IoDispatcher
 import com.petruccini.pokephone.data.api.services.pokemon_details.PokemonDetailsService
 import com.petruccini.pokephone.data.api.services.pokemon_details.model.toPokemonDetails
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RemotePokemonDetailsDataSource @Inject constructor(
-    private val pokemonDetailsService: PokemonDetailsService
+    private val pokemonDetailsService: PokemonDetailsService,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
 
-    suspend fun fetchPokemonDetails(pokemonName: String) =
+    suspend fun fetchPokemonDetails(pokemonName: String) = withContext(dispatcher) {
         pokemonDetailsService.getPokemonDetails(pokemonName).body()?.toPokemonDetails()
+    }
 }
--- app/src/main/java/com/petruccini/pokephone/data/data_sources/remote/RemotePokemonListDataSource.kt
+++ app/src/main/java/com/petruccini/pokephone/data/data_sources/remote/RemotePokemonListDataSource.kt
@@ -1,13 +1,19 @@
 package com.petruccini.pokephone.data.data_sources.remote
 
+import com.petruccini.pokephone.config.IoDispatcher
 import com.petruccini.pokephone.data.api.services.pokemon_list.PokemonListService
 import com.petruccini.pokephone.data.api.services.pokemon_list.model.toPokemonList
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RemotePokemonListDataSource @Inject constructor(
-    private val pokemonListService: PokemonListService
+    private val pokemonListService: PokemonListService,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
 
-    suspend fun fetchPokemonList(offset: Int, limit: Int) =
+    suspend fun fetchPokemonList(offset: Int, limit: Int) = withContext(dispatcher) {
         pokemonListService.getPokemons(offset, limit).body()?.toPokemonList()
+    }
 }
